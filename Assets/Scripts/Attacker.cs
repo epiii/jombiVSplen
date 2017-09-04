3eff8caffccb739c8fed5a5f206cfdc428a25a9a
epiii <gue.elfrianto@gmail.com>
2017-09-05 01:45:11 +0700

    ok

---
+++ Assets/Scripts/Attacker.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attacker : MonoBehaviour {
+	public bool isPlayer = true;
+	private bool isMove  = true;
+	public int attact    = 100;
+	public int defense   = 200;
+	
+	[HideInInspector]
+	public int underAttact;
+	private float posYLawan;
+	private string nameTagLawan;
+	private float timer  = 0;
+	private bool isCari  = false;
+
+	// Use this for initialization
+	void Start(){
+		if (isPlayer){
+			nameTagLawan = "Enemy";
+		}else{
+			nameTagLawan = "Player";
+		}
+	}
+
+	// Update is called once per frame
+	void FixedUpdate(){
+		if (isPlayer){ // player
+			if (isMove){ // moving
+				transform.position += transform.right * Time.deltaTime * 0.5f;
+				
+				if (transform.position.y > (posYLawan + 0.1f) && isCari){
+					transform.position = new Vector2(transform.position.x, (transform.position.y - Time.deltaTime));
+				}
+
+				if (transform.position.y < (posYLawan - 0.1f) && isCari){
+					transform.position = new Vector2(transform.position.x, (transform.position.y + Time.deltaTime));
+				}
+			} else { // stop / hit something
+				// attact
+				timer += Time.deltaTime;
+				if (timer > 0.6f) {
+					defense -= underAttact;
+					transform.localScale = new Vector3(1, 1f);
+					timer = 0;
+				} else if (timer > 0.5f) {
+					transform.localScale = new Vector3(1, 1.2f);
+				}
+			}
+		} else { // enemy
+			if (isMove) { // move forward
+				transform.position -= transform.right * Time.deltaTime * 0.5f;
+				if (transform.position.y > (posYLawan + 0.1f) && isCari) {
+					transform.position = new Vector2(transform.position.x, (transform.position.y - Time.deltaTime));
+				}
+				if (transform.position.y < (posYLawan - 0.1f) && isCari) {
+					transform.position = new Vector2(transform.position.x, (transform.position.y + Time.deltaTime));
+				}
+			} else { // hit something / attack others
+				// attact
+				timer += Time.deltaTime;
+				if (timer > 0.6f) {
+					defense -= underAttact;
+					transform.localScale = new Vector3(1, 1f);
+					timer = 0;
+				} else if (timer > 0.5f) {
+					transform.localScale = new Vector3(1, 1.2f);
+				}
+			}
+		}
+
+		if (defense <= 0) { 
+			Destroy(gameObject);
+		}
+
+		if (transform.position.x > 9 || transform.position.x < -9) {
+			Destroy(gameObject);
+		}
+	}
+
+	private void OnCollisionStay2D(Collision2D collision) {
+		if (collision.transform.tag.Equals(nameTagLawan) && isMove) {
+			isMove = false;
+			Attacker m = collision.gameObject.GetComponent<Attacker>();
+
+			if (m != null) 
+				m.underAttact = attact;
+
+			// ===========
+			Defender d = collision.gameObject.GetComponent<Defender>();
+			if (d != null)
+				d.underAttact = attact;
+		}
+	}
+	
+	public void OnTriggerStay2D(Collider2D collision) {
+		if (collision.transform.tag.Equals(nameTagLawan)) {
+			isCari = true;
+			posYLawan = collision.transform.position.y;
+		}
+	}
+
+	public void OnCollisionExit2D(Collision2D collision) {
+		isMove = true;
+		transform.localScale = new Vector3(1, 1f);
+	}
+}
---
+++ Assets/Scripts/TowerDestroy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerDestroy : MonoBehaviour {
+	public bool isPlayer = true;
+	
+	private void OnDestroy() {
+		if (!Data.isGameOver){
+			if (isPlayer) { // plyer
+				Data.isGameOver = true;
+				Data.isComplate = false;
+				Debug.Log("Player Lost");
+			} else { // enemy 
+				Data.isGameOver = true;
+				Data.isComplate = true;
+				Debug.Log("Player Win");
+			}
+		}
+	}
+}
